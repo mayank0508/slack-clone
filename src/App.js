bacb80e832e7fa2b60867acc918c9dbc8e6f69c3
mayank0508 <thakurmayank0508@gmail.com>
2021-06-18 23:32:19 +0530

    Header added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HI BABY 🥶</h1>
+      {/* Header */}
+      <div className="header">
+        
+      </div>
+      {/* Sidebar */}
+      {/* React Router -> for code */}
+    </div>
+  );
+}
+
+export default App;
