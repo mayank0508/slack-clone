68afee9d24ff72558b647383aadabdc30b032f6b
mayank0508 <thakurmayank0508@gmail.com>
2021-06-20 04:09:06 +0530

    Chat work has been started 📈📈

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Sidebar from './Sidebar/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Chat from './Chat/Chat';
 
 function App() {
   return (
     <div className="app">
-      <h1>HI BABY 🥶</h1>
+      <Router>
       {/* Header */}
       <Header />
       <div className="app__body">
         {/* Sidebar */}
         <Sidebar />
+        {/* React Router -> for code */}
+        <Switch>
+          <Route path="/room/:roomId">
+            <Chat />
+          </Route>
+          <Route path="/">
+            <h1>Welcome</h1>
+          </Route>
+        </Switch>
       </div>
-      {/* React Router -> for code */}
+      </Router>
     </div>
   );
 }
